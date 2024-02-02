af2d41fa15f11e38e910efecf22e7ac39fdddd12
SHEZ <shezcode@gmail.com>
2024-02-02 13:51:31 +0100

    devolverMovimientos validos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         Tablero tablero = new Tablero();
         tablero.printTablero();
+        System.out.print("Introduce la pieza a mover (Eje Y): ");
+        String inputX = scanner.nextLine();
+        System.out.print("Introduce la pieza a mover (Eje X): ");
+        String inputY = scanner.nextLine();
+        int[] posInicial = {Integer.parseInt(inputX), Integer.parseInt(inputY)};
+        int[][] posValidas = tablero.devolverMovimientos(posInicial);
+        System.out.println(Arrays.deepToString(posValidas));
     }
 }
--- src/Tablero.java
+++ src/Tablero.java
@@ -1,21 +1,25 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tablero {
    private final int DIMENSION= 8;
    public char[][] tablero = new char[DIMENSION][DIMENSION];
-   private int[][] posNegras = {
+   public int[][] posNegras = {
            {0, 1}, {0, 3}, {0, 5}, {0, 7},
            {1, 0}, {1, 2}, {1, 4}, {1, 6},
            {2, 1}, {2, 3}, {2, 5}, {2, 7}
    };
-   private int[][] posBlancas = {
+
+   public int[][] posBlancas = {
            {5, 0}, {5, 2}, {5, 4}, {5, 6},
            {6, 1}, {6, 3}, {6, 5}, {6, 7},
            {7, 0}, {7, 2}, {7, 4}, {7, 6},
    };
+
    public Tablero(){
       insertarL();
-      insertPiezas();
+      rellenarPiezas();
    }
 
    public void insertarL(){
@@ -39,6 +43,29 @@ public class Tablero {
          }
       }
    }
+   private void rellenarPiezas() {
+      for (int i=0; i<DIMENSION; i++) {
+
+         for (int j=0; j<DIMENSION; j++) {
+            if (i%2 == 0 && j%2==1 && i<3) {
+               tablero[i][j] = 'N';
+            }
+
+            if (i%2 == 1 && j%2 == 0 && i<3) {
+               tablero[i][j] = 'N';
+            }
+
+            if (i%2 == 0 && j%2==1 && i>4) {
+               tablero[i][j] = 'B';
+            }
+
+            if (i%2 == 1 && j%2 == 0 && i>4) {
+               tablero[i][j] = 'B';
+            }
+
+         }
+      }
+   }
 
    void printTablero(){
        for (char[] fila : this.tablero){
@@ -47,4 +74,63 @@ public class Tablero {
        System.out.println();
    }
 
+   int[][] devolverMovimientos(int[] posicionInicial){
+      ArrayList<int[]> result = new ArrayList<>();
+
+      //comprobar si posicion inicial es de Blancas
+      if (tablero[posicionInicial[0]][posicionInicial[1]] == 'B'){
+         if (tablero[posicionInicial[0] - 1][posicionInicial[1] - 1] == 'L'){
+            int[] posicionValida = {posicionInicial[0] - 1, posicionInicial[1] - 1};
+            result.add(posicionValida);
+         }
+         if (tablero[posicionInicial[0] - 1][posicionInicial[1] + 1] == 'L'){
+            int[] posicionValida = {posicionInicial[0] - 1, posicionInicial[1] + 1};
+            result.add(posicionValida);
+         }
+      }
+
+      //comprobar si posicion inicial es de Negras
+      if (tablero[posicionInicial[0]][posicionInicial[1]] == 'N'){
+         if (tablero[posicionInicial[0] + 1][posicionInicial[1] - 1] == 'L'){
+            int[] posicionValida = {posicionInicial[0] + 1, posicionInicial[1] - 1};
+            result.add(posicionValida);
+         }
+         if (tablero[posicionInicial[0] + 1][posicionInicial[1] + 1] == 'L'){
+            int[] posicionValida = {posicionInicial[0] + 1, posicionInicial[1] + 1};
+            result.add(posicionValida);
+         }
+      }
+
+      //pasar el ArrayList a int[][] y asi devolver las posiciones validas
+      int[][] resultArray = new int[result.size()][2];
+      for (int i = 0; i < result.size(); i++) {
+         resultArray[i] = result.get(i);
+      }
+
+      return resultArray;
+   }
+
+   public char[][] getTablero() {
+      return tablero;
+   }
+
+   public void setTablero(char[][] tablero) {
+      this.tablero = tablero;
+   }
+
+   public int[][] getPosNegras() {
+      return posNegras;
+   }
+
+   public void setPosNegras(int[][] posNegras) {
+      this.posNegras = posNegras;
+   }
+
+   public int[][] getPosBlancas() {
+      return posBlancas;
+   }
+
+   public void setPosBlancas(int[][] posBlancas) {
+      this.posBlancas = posBlancas;
+   }
 }
